c020fabc3987ab531fdef77210de5c221b4dc979
PurpleToti <arthur.ergin.16@gmail.com>
2023-12-28 16:47:46 +0100

    base files

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"echoauthtest/auth"
+	"echoauthtest/controllers"
+
+	"github.com/golang-jwt/jwt/v4"
+	echojwt "github.com/labstack/echo-jwt"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	adminGroup := e.Group("/admin")
+
+	// Read more about JWT Middleware here: https://echo.labstack.com/middleware/jwt
+	adminGroup.Use(echojwt.WithConfig(echojwt.Config{
+		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+			return &auth.Claims{ // this needs to be pointer to json unmarshalling to work
+			}
+		},
+		SigningKey:   []byte(auth.GetJWTSecret()),
+		TokenLookup:  "cookie:access-token", // "<source>:<name>"
+		ErrorHandler: auth.JWTErrorChecker,
+	}))
+
+	adminGroup.GET("", controllers.Admin())
+
+	e.GET("/user/signin", controllers.SignInForm()).Name = "userSignInForm"
+	e.POST("/user/signin", controllers.SignIn())
+
+	e.Logger.Fatal(e.Start(":8777"))
+}
