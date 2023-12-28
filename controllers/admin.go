7cb7c65a70491b594d6fc70654f32677250fa87a
PurpleToti <arthur.ergin.16@gmail.com>
2023-12-28 18:16:16 +0100

    changement valeur affiché pour l'utilisateur connecté

--- controllers/admin.go
+++ controllers/admin.go
@@ -11,6 +11,6 @@ func Admin() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		// Gets user cookie.
 		userCookie, _ := c.Cookie("user")
-		return c.String(http.StatusOK, fmt.Sprintf("Hi, %s! You have been authenticated!", userCookie.Name))
+		return c.String(http.StatusOK, fmt.Sprintf("Hi, %s! You have been authenticated!", userCookie.Value))
 	}
 }
